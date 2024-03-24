d72ab9692b70925604d468ac1986bcccd3c35cdf
Maddie <maddieeeperryyy@gmail.com>
2024-03-24 12:56:56 +0000

    Fixed Layout
    Changed values of align enums and updated and documented Layout

--- Layout.cs
+++ Layout.cs
@@ -61,12 +61,12 @@ public class Layout : MonoBehaviour, IUIElement
 
     public List<Vector3> GetPositions()
     {
+        //Calculate world bounds
         RectTransform rect = GetComponent<RectTransform>();
-
         Vector3 min = Vector3.positiveInfinity;
         Vector3 max = Vector3.negativeInfinity;
         Vector3[] corners = new Vector3[4];
-
+        
         rect.GetWorldCorners(corners);
 
         foreach (Vector3 corner in corners)
@@ -81,58 +81,54 @@ public class Layout : MonoBehaviour, IUIElement
         Bounds bounds = new Bounds();
         bounds.SetMinMax(min, max);
 
-        int dimension = (int)_alignBy;
-
-        float _wrapAfter = float.MaxValue;
-        if (_wrap)
-        {
-            _wrapAfter = bounds.size[dimension];
-        }
+        int dimension = Math.Min((int)_alignBy, 1);
+        Debug.Log(dimension);
+        float _wrapAfter = _wrap ? bounds.size[dimension] : float.MaxValue;
 
         float runningOffset = 0f;
         List<List<RectTransform>> layoutGrouping = new List<List<RectTransform>>() { new List<RectTransform>() };
         List<Vector2> groupingMaxSize = new List<Vector2>() { Vector2.zero };
         List<Vector3> positions = new List<Vector3>();
 
-        //Loop through elements to create _wrap groups
+        //loop through elements to create _wrap groups
         for (int i = 0; i < _layoutElements.Count; i++)
         {
             Bounds elementBounds = _layoutElements[i].GetBoundsWithChildren(_ignoreObjects, transform.rotation);
-            runningOffset += elementBounds.size[dimension] + _padding[dimension];
+            runningOffset += elementBounds.size[dimension];
 
-            if (runningOffset > _wrapAfter + _padding[dimension]) //larger than _wrap limit
+            if (runningOffset > _wrapAfter) //larger than _wrap limit
             {
-                if (runningOffset == elementBounds.size[dimension] + _padding[dimension]) //Do not _wrap because there is only one element in group
+                if (runningOffset == elementBounds.size[dimension]) //create new group but do not place element in it because there is only one
                 {
                     layoutGrouping.Last().Add(_layoutElements[i]);
-                    groupingMaxSize[groupingMaxSize.Count - 1] = groupingMaxSize[groupingMaxSize.Count - 1].ChangeValue((1 - dimension), Math.Max(groupingMaxSize.Last()[1 - dimension], elementBounds.size[1 - dimension]));
-                    groupingMaxSize[groupingMaxSize.Count - 1] = groupingMaxSize[groupingMaxSize.Count - 1].ChangeValue(dimension, runningOffset - _padding[dimension]);
-                    runningOffset = 0f;
+                    groupingMaxSize[groupingMaxSize.Count - 1] = groupingMaxSize.Last().ChangeValue((1 - dimension), Math.Max(groupingMaxSize.Last()[1 - dimension], elementBounds.size[1 - dimension]));
+                    groupingMaxSize[groupingMaxSize.Count - 1] = groupingMaxSize.Last().ChangeValue(dimension, runningOffset - _padding[dimension]);
                     layoutGrouping.Add(new List<RectTransform>());
                     groupingMaxSize.Add(Vector2.zero);
+                    runningOffset = 0f;
                     continue;
                 }
-                //Wrap and place element in new group
-                runningOffset = elementBounds.size[dimension] + _padding[dimension];
+                //wrap and place element in new group
                 layoutGrouping.Add(new List<RectTransform>() { _layoutElements[i] });
-                groupingMaxSize.Add(Vector2.zero);
-                groupingMaxSize[groupingMaxSize.Count - 1] = groupingMaxSize[groupingMaxSize.Count - 1].ChangeValue((1 - dimension), Math.Max(groupingMaxSize.Last()[1 - dimension], elementBounds.size[1 - dimension]));
-                groupingMaxSize[groupingMaxSize.Count - 1] = groupingMaxSize[groupingMaxSize.Count - 1].ChangeValue(dimension, runningOffset - _padding[dimension]);
+                groupingMaxSize.Add(new Vector2(elementBounds.size.x, elementBounds.size.y)); //leave current groupingMaxSize as current element will not affect it; set new groupingMaxSize to element size
+                runningOffset = elementBounds.size[dimension] + _padding[dimension];
                 continue;
             }
-            //smaller than _wrap limit
-            layoutGrouping.Last().Add(_layoutElements[i]);
-            groupingMaxSize[groupingMaxSize.Count - 1] = groupingMaxSize[groupingMaxSize.Count - 1].ChangeValue((1 - dimension), Math.Max(groupingMaxSize.Last()[1 - dimension], elementBounds.size[1 - dimension]));
-            groupingMaxSize[groupingMaxSize.Count - 1] = groupingMaxSize[groupingMaxSize.Count - 1].ChangeValue(dimension, runningOffset - _padding[dimension]);
+            //smaller than wrap limit
+            layoutGrouping[groupingMaxSize.Count - 1].Add(_layoutElements[i]);
+            groupingMaxSize[groupingMaxSize.Count - 1] = groupingMaxSize.Last().ChangeValue((1 - dimension), Math.Max(groupingMaxSize.Last()[1 - dimension], elementBounds.size[1 - dimension])); //set current groupingMaxSize component in perpendicular direction to max of current value and current element size in perpendicular direction
+            groupingMaxSize[groupingMaxSize.Count - 1] = groupingMaxSize.Last().ChangeValue(dimension, runningOffset); //set current groupingMaxSize component in direction to sum of the sizes of each element in current grouping + padding between each
+            runningOffset += _padding[dimension];
         }
 
-        //Loop through layout groupings to calculate positions
+        //loop through layout groupings to calculate total size
         _size = Vector2.zero;
         foreach (Vector2 groupSize in groupingMaxSize)
         {
-            _size = _size.ChangeValue((1 - dimension), _size[1 - dimension] + groupSize[1 - dimension]);
-            _size = _size.ChangeValue(dimension, Math.Max(_size[dimension], groupSize[dimension]));
+            _size = _size.ChangeValue((1 - dimension), _size[1 - dimension] + groupSize[1 - dimension] + _padding[1 - dimension]); //set current groupingMaxSize component in perpendicular direction to sum of current value and group size and padding component in perpendicular direction
+            _size = _size.ChangeValue(dimension, Math.Max(_size[dimension], groupSize[dimension])); //set size component in direction to max of current value and gourp size component in direction
         }
+        _size = _size.ChangeValue((1 - dimension), _size[1 - dimension] - _padding[1 - dimension]); //remove unnecessary padding
 
         float groupOffset = 0f;
         if (align[1 - dimension] * invert[1 - dimension] == -1)
--- UI.cs
+++ UI.cs
@@ -1,4 +1,4 @@
-public enum XAlign { Left, Center, Right }
-public enum YAlign { Bottom, Center, Top }
-public enum ZAlign { Back, Center, Front }
+public enum XAlign { Left = -1, Center = 0, Right = 1 }
+public enum YAlign { Bottom = -1, Center = 0, Top = 1 }
+public enum ZAlign { Back = -1, Center = 0, Front = 1 }
 public enum Dimension { x = 0, y = 1, z = 2 }
